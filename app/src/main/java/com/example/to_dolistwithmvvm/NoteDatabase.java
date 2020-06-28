244c6a98bdbec1e517fd147fec30e7c5264b0d56
Dell <Spidey-N17>
2020-06-29 04:25:14 +0530

    code101

--- app/src/main/java/com/example/to_dolistwithmvvm/NoteDatabase.java
+++ app/src/main/java/com/example/to_dolistwithmvvm/NoteDatabase.java
@@ -30,7 +30,7 @@ public abstract class NoteDatabase extends RoomDatabase {
         @Override
         public void onCreate(@NonNull SupportSQLiteDatabase db) {
             super.onCreate(db);
-            new PopulatedbAsyncTask(instance);
+            new PopulatedbAsyncTask(instance).execute();
         }
     };
 
