7da686ae2085423349d30da2e18fa0abde074d2e
Dell <Spidey-N17>
2020-07-15 03:45:26 +0530

    nothing

--- app/src/main/java/com/example/to_dolistwithmvvm/MainActivity.java
+++ app/src/main/java/com/example/to_dolistwithmvvm/MainActivity.java
@@ -25,6 +25,7 @@ public class MainActivity extends AppCompatActivity {
     private NoteViewModel noteViewModel;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         FloatingActionButton floatingActionButton = findViewById(R.id.floatingActionButton);
